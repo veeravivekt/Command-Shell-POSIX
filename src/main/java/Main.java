182afa8a1fdacb111bdf6906360471b4ab74ab58
veeravivekt <veeravivek.telagani@sjsu.edu>
2025-01-07 18:56:32 +0530

    builtin type command

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
+    private static final List<String> BUILTINS = Arrays.asList("echo", "exit", "type");
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
 
@@ -12,9 +15,19 @@ public class Main {
                 break;
             } else if (input.startsWith("echo ")) {
                 System.out.println(input.substring("echo ".length()));
+            } else if (input.startsWith("type ")){
+                handleTypeCommand(input.substring(5));
+
             } else {
                 System.out.println(input + ": command not found");
             }
+        }
+    }
+    private static void handleTypeCommand(String command) {
+        if (BUILTINS.contains(command)) {
+            System.out.println(command + " is a shell builtin");
+        } else {
+            System.out.println(command + ": not found");
         }
     }
 }
